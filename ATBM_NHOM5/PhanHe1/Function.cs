336cfadc8a97f11dd6bc048ae3a7f2cdea929ce3
Yooni <20127251@student.hcmus.edu.vn>
2023-04-01 20:07:14 +0700

    update function.cs/ Program.cs

---
+++ ATBM_NHOM5/PhanHe1/Function.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+namespace PhanHe1
+{
+    public class Functions
+    {
+        public static OracleConnection Con;
+
+        private static string host_name = @"LAPTOP-LR15QBT2"; // Minh Host
+
+        public static void InitConnection(String username, String password)
+        {
+            String connectionString = @"Data Source=" + host_name + ";User ID=" + username + ";Password=" + password + "";
+
+            Con = new OracleConnection();
+            Con.ConnectionString = connectionString;
+
+            try
+            {
+                //Mở kết nối
+                Con.Open();
+
+                ////Kiểm tra kết nối
+                if (Con.State == ConnectionState.Open)
+                {
+                    MessageBox.Show("Kết nối DB thành công");
+                }
+
+            }
+            catch (OracleException ex)
+            {
+                Con = null;
+                throw new Exception(ex.Message);
+                MessageBox.Show("Không thể kết nối với DB");
+            }
+        }
+    }
+}
